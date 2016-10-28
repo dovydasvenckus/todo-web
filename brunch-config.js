51fcf5ab154b71fc92509ff25989aeb6043f0ce9
Dovydas Venckus <dovydas.venckus@live.com>
2016-10-28 22:01:51 +0300

    Split vendor code to separate js file

--- brunch-config.js
+++ brunch-config.js
@@ -1,14 +1,19 @@
 // See http://brunch.io for documentation.
 module.exports = {
-  files: {
-    javascripts: {joinTo: 'app.js'},
-    stylesheets: {joinTo: 'app.css'},
-    templates: {joinTo: 'app.js'}
-  },
-  npm: {
-    globals: {
-      $: 'jquery',
-      Backbone: 'backbone'
+    files: {
+        javascripts: {
+            joinTo: {
+                'app.js': /^app/,
+                'vendor.js': /^(?!app)/
+            }
+        },
+        stylesheets: {joinTo: 'app.css'},
+        templates: {joinTo: 'app.js'}
+    },
+    npm: {
+        globals: {
+            $: 'jquery',
+            Backbone: 'backbone'
+        }
     }
-  }
 }
