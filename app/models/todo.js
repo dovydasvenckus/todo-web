758604790b17f5564e259135b801d82a2d79e88c
Dovydas Venckus <dovydas.venckus@live.com>
2016-10-30 00:06:17 +0300

    Added checkbox near every todo

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,7 +1,19 @@
 module.exports = Backbone.Model.extend({
-  defaults: {
-    id: '',
-    title: '',
-    done: false
-  }
+    defaults: {
+        id: '',
+        title: '',
+        isDone: false
+    },
+
+    toggle: function () {
+        var self = this;
+        var isDone = self.get('isDone');
+        $.ajax({
+            type: "POST",
+            url: "/api/todo/toggle/" + self.get("id"),
+            success: function () {
+                self.set("isDone", !isDone);
+            },
+        });
+    }
 });
--- app/views/todo/entry.js
+++ app/views/todo/entry.js
@@ -1,10 +1,17 @@
 module.exports = Backbone.View.extend({
     tagName: 'tr',
-
     template: require('templates/todo/entry.jade'),
+    events: {
+        'click .isDone': 'toggle',
+    },
+
 
     render: function() {
         this.$el.html(this.template(this.model.attributes));
         return this;
+    },
+    
+    toggle: function () {
+        this.model.toggle();
     }
 });
